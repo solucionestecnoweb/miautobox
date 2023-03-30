e52d0d2530f75b508d566fe18a1375795019dcd7
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-03-30 17:43:03 -0400

    pos_stock sin fabricacion

--- POS_VENTA/pos_stocks/__init__.py
+++ POS_VENTA/pos_stocks/__init__.py
@@ -13,6 +13,6 @@ def pre_init_check(cr):
     from odoo.exceptions import Warning
     version_info = common.exp_version()
     server_serie =version_info.get('server_serie')
-    if server_serie!='15.0':
+    if server_serie!='14.0':
         raise Warning('Module support Odoo series 15.0 found {}.'.format(server_serie))
     return True
